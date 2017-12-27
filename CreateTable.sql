d4e950aa7137c3814e772c5902d7c27305a10d01
gman gman <gman@gman-Ins>
2017-12-26 23:16:07 -0600

    Creacion de tablas para el proyecto

--- CreateTable.sql
+++ CreateTable.sql
@@ -1,1 +1,13 @@
-create table();
+-- creamos un schema para el usuairo
+CREATE SCHEMA usuario;
+
+CREATE TABLE (
+    id int NOT NULL,
+    nombre varchar(255) NOT NULL,
+    epellido varchar(255),
+    fecha_nacimiento date,
+    correo varchar(255),
+    nombre_usuario varchar(255)
+    contrasena varchar(500)
+    CONSTRAINT PK_Person PRIMARY KEY (id)
+); 
